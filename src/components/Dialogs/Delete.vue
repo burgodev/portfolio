ffe1fafa16ffa1e0b6cde6d62d00ad617ca2c935
filipe <burgodev@gmail.com>
2019-12-20 18:45:29 -0300

    Components Library²

---
+++ src/components/Dialogs/Delete.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-dialog v-model="dialog" max-width="400px">
+    <v-card class="mx-auto" max-width="400" outlined>
+      <v-card-title>{{title}}</v-card-title>
+      <v-divider class="mb-5" />
+
+      <v-card-text>{{text}}</v-card-text>
+
+      <v-card-actions class="justify-end">
+        <v-btn text @click="cancel()">Cancelar</v-btn>
+        <v-btn text @click="confirm()">Confirmar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "Delete",
+
+  data () {
+      return {
+        dialog: false,
+        title: 'Excluir Usuário',
+        text: 'Você tem certeza que deseja excluir este usuário?'
+      }
+    },
+
+  methods: {
+    open(title, text) {
+        if(title){
+           
+            this.title = title;
+        }
+
+        if(text){
+            this.text = text;
+        }
+
+        this.dialog = true;
+    },
+
+    cancel(){
+        this.dialog = false;
+    },
+
+    confirm(){
+        this.deleteItem();
+
+    },
+
+    deleteItem(){
+      /*  try {
+            let ret = await axios({
+                baseURL: `${this.config.protocol}://${this.config.baseUrl}:${this.config.port}/`,
+                url: `/api/admin/user/delete/${userId}`,
+                method: 'delete',
+                headers: await Utils.handlerHead(),
+            });
+            return ret.data;
+        }
+        catch (e) {
+            return e;
+        }*/
+
+        this.dialog = false;
+    },
+
+
+  }
+};
+</script>
+
+<style scoped>
+.justify-end {
+  justify-content: flex-end;
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <v-menu bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn v-on="on" fab icon outlined fixed>
+          <v-icon>menu</v-icon>
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item v-for="(item, index) in menu" :key="index" @click="openMenuItem(index)">
+          <v-list-item-title>
+            <v-icon>{{item.icon}}</v-icon>
+            {{ item.title }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+    <Delete ref="Delete" />
+    <Gallery v-if="gallery"/>
+  </div>
+</template>
+
+<script>
+  import Delete from "./Dialogs/Delete";
+  import Gallery from "./Gallery";
+export default {
+  name: "Menu",
+  components:{
+    Gallery,
+    Delete
+  },
+
+  data() {
+    return {
+      menu: [
+        { title: "Delete", icon: "delete" },
+        { title: "Perfil", icon: "person" },
+        { title: "Anuncie", icon: "" },
+        { title: "Termos de Uso", icon: "" }
+      ],
+
+      gallery: false,
+    };
+  },
+
+  methods: {
+    openMenuItem(index) {
+      this.closeAll();
+
+      switch (index) {
+        case 0:
+          this.openDelete();
+          break;
+        case 1:
+          this.showGallery();
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+      }
+    },
+
+
+    openDelete() {
+      this.$refs.Delete.open('Excluir Anúncio', 'Você tem certeza que deseja excluir este anúncio?');
+    },
+
+    showGallery(){
+      this.gallery = true;
+    },
+
+    closeAll(){
+      this.gallery = false;
+    },
+
+  },
+
+};
+</script>
+
+<style scoped >
+</style>
